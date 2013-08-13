b31d53b6622365b38bc3d7c9b88e9b52fcd7ca05
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2013-08-12 22:36:56 -0400

    wip: Adding update logic for build infrastructure

---
+++ CMake/cmake_modules/core.cmake
@@ -0,0 +1,218 @@
+cmake_minimum_required(VERSION 2.8)
+
+# VTK, CDAT, NetCDF (with opendap support), NetCDF4Python, use system python
+
+#/////////////////////////////////////////////////////////////////////////////
+#
+# Marcro to extract key-value from the template
+#
+#/////////////////////////////////////////////////////////////////////////////
+macro(__parse_extract_template template)
+  set(_vars "")
+  set(_extra "")
+
+  foreach(pair ${template})
+    string(REGEX MATCH "^[^=]+" varname "${pair}")
+    string(REGEX REPLACE "^[^=]+=" "" token "${pair}")
+    if("x_${varname}" STREQUAL "x_" OR "x_${token}" STREQUAL "x_")
+      message(FATAL_ERROR "extract_args: bad variable/token pair '${pair}'")
+    endif()
+    set(${varname} "")
+    set(__var_for_token_${token} "${varname}")
+    list(APPEND _vars "${varname}")
+  endforeach()
+endmacro()
+
+#/////////////////////////////////////////////////////////////////////////////
+#
+# Function to extract key-value+extra from the argument list
+#
+#/////////////////////////////////////////////////////////////////////////////
+function(extract_args template)
+  __parse_extract_template("${template}")
+  set(_var "")
+
+  foreach(arg ${ARGN})
+    # Skip empty args
+    if(NOT "x_${arg}" STREQUAL "x_")
+      # Test if arg is a token
+      if(NOT "x_${__var_for_token_${arg}}" STREQUAL "x_")
+        set(_var "${__var_for_token_${arg}}")
+      # Not a token; test if we have a current token
+      elseif(NOT "x_${_var}" STREQUAL "x_")
+        # Add arg to named list
+        list(APPEND ${_var} "${arg}")
+      else()
+        # Add arg to leftovers list
+        list(APPEND _extra "${arg}")
+      endif()
+    endif()
+  endforeach()
+
+  # Raise lists to parent scope
+  foreach(varname ${_vars})
+    set(${varname} "${${varname}}" PARENT_SCOPE)
+  endforeach()
+
+  # Shift ARGN
+  set(ARGN "${_extra}" PARENT_SCOPE)
+endfunction()
+
+#/////////////////////////////////////////////////////////////////////////////
+#
+# Function to add a package to the superbuild.
+#
+# DEFAULT: ON OFF SYSTEM
+#
+#/////////////////////////////////////////////////////////////////////////////
+function(add_sb_package)
+  extract_args("_name=NAME;_version=VERSION;_groups=GROUPS;_default=DEFAULT" ${ARGN})
+  message("name is ${_name}")
+  message("version is ${_version}")
+  message("groups are ${_groups}")
+  message("default is ${_default}")
+
+  # Create convenient names
+  string(TOUPPER ${_name} _uc_package_name)
+  string(TOLOWER ${_name} _lc_package_name)
+  set(_use_system_${_lc_package_name} PARENT_SCOPE)
+  set(_build_package_${_lc_package_name} PARENT_SCOPE)
+
+  set(_use_system_${_lc_package_name} OFF)
+  set(_build_package_${_lc_package_name} ON)
+
+  if ("${_default}" STREQUAL "ON")
+    set(_build_package_${_lc_package_name} ON)
+  elif ("${_default}" STREQUAL "OFF")
+    set(_build_package_${_lc_package_name} OFF)
+  elif ("${_default}" STREQUAL "SYSTEM")
+    set(_use_system_${_lc_package_name} ON)
+    set(_build_package_${_lc_package_name} OFF)
+  endif()
+
+  # Find all the groups this package belongs to and then
+  # append this package to each group
+  foreach(group ${_groups})
+    if(NOT DEFINED _group_names)
+      set(_group_names)
+    endif()
+
+    if(NOT DEFINED _group_pkgs)
+      set(_group_pkgs)
+    endif()
+
+    list(FIND _group_names ${group} ${group}_exists)
+    if ("${${group}_exists}" STREQUAL "-1")
+      message("[sb:info] Appending group name ${_group_names}")
+      list(APPEND _group_names ${group})
+    endif()
+
+    # Check if the package is already a part of the group or not
+    # if it is skip adding it or else add it to the list of packages
+    # group will enable
+    list(FIND _${group}_pkgs ${_name} ${_name}_exists_in_${group})
+    if(${${_name}_exists_in_${group}} STREQUAL "-1")
+      list(APPEND _${group}_pkgs ${_name})
+    endif()
+
+    set(_group_names ${_group_names} PARENT_SCOPE)
+    set(_${group}_pkgs ${_${group}_pkgs} PARENT_SCOPE)
+
+    message("[sb:debug] group pkgs ${_${group}_pkgs}")
+  endforeach()
+
+  option(SB_BUILD_${_uc_package_name} "${message}" ${_build_package_${_lc_package_name}})
+  mark_as_advanced(SB_BUILD_${_uc_package_name})
+
+  option(SB_USE_SYSTEM_${_uc_package_name} "${message}" ${_use_system_${_lc_package_name}})
+  mark_as_advanced(SB_USE_SYSTEM_${_uc_package_name})
+endfunction()
+
+#/////////////////////////////////////////////////////////////////////////////
+#
+# Function to create the superbuild
+#
+#/////////////////////////////////////////////////////////////////////////////
+function execute()
+  set(_external_packages)
+  create_package_and_groups()
+  resolve_package_dependencies()
+  create_build_list()
+endfunction()
+
+#/////////////////////////////////////////////////////////////////////////////
+#
+# Helper macro to gather list of packages and groups
+#
+#/////////////////////////////////////////////////////////////////////////////
+macro create_package_and_groups()
+  foreach(group ${_group_names})
+    message("[sb:debug] Group is ${group} with pkgs ${_${group}_pkgs}")
+    option(SB_ENABLE_${group} "Enable group ${group}" ON)
+
+    # If a group is ON, then eanble all of its packages or else don't build
+    # any of its packages (unless this is overriden by dependency walker.
+    if (SB_ENABLE_${group})
+      foreach(package ${_${group}_pkgs})
+        set_property(CACHE SB_USE_SYSTEM_${uc_package} PROPERTY VALUE ${_use_system_${_lc_package_name}})
+        set_property(CACHE SB_BUILD_${uc_package} PROPERTY VALUE ${_build_package_${_lc_package_name}})
+
+        # Append this package to the global list for all of the packages
+        # that will be built by this instance
+        # TODO Check if the package already exists
+        list(APPEND _external_packages "${package}")
+      endforeach()
+    else()
+      foreach(package ${_${group}_pkgs})
+        set_property(CACHE SB_USE_SYSTEM_${uc_package} PROPERTY VALUE OFF)
+        set_property(CACHE SB_BUILD_${uc_package} PROPERTY VALUE OFF)
+      endforeach()
+    endif()
+  endforeach()
+endmacro()
+
+#/////////////////////////////////////////////////////////////////////////////
+#
+# Resolve package dependencies
+#
+#/////////////////////////////////////////////////////////////////////////////
+macro(resolve_package_dependencies)
+  include(TopologicalSort)
+  message("[sb:debug] Packages: ${_external_packages}")
+  foreach(package ${_external_packages})
+    string(TOLOWER ${package} lc_package_name)
+    include("${lc_package_name}_deps")
+  endforeach()
+
+  topological_sort(_external_packages "" "_deps")
+
+  foreach(package ${_external_packages})
+    do_resolve_package_deps(${package})
+  endforeach()
+endmacro()
+
+macro(do_resolve_package_deps package_name)
+  string(TOUPPER ${package_name} uc_package_name)
+  string(TOLOWER ${package_name} lc_package_name)
+
+  if (SB_BUILD_${uc_package})
+    foreach(dep ${${package_name}_deps})
+      string(TOUPPER ${dep} uc_dep)
+      if(NOT CDAT_USE_SYSTEM_${uc_dep} AND NOT CDAT_BUILD_${uc_dep})
+        set(CDAT_BUILD_${uc_dep} ON CACHE BOOL "" FORCE)
+        message("[INFO] Setting build package -- ${dep} ON -- as required by ${package_name}")
+      endif()
+      if(NOT DEFINED CDAT_USE_SYSTEM_${uc_dep})
+        mark_as_advanced(CDAT_BUILD_${uc_dep})
+      endif()
+    endforeach()
+  endif()
+endmacro()
+
+#/////////////////////////////////////////////////////////////////////////////
+#
+# Create final build list
+#
+#/////////////////////////////////////////////////////////////////////////////
+macro(create_build_list)
+endmacro()
