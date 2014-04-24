4d2003ba61c2805c374de96b8b99fdbd9a69ae24
Chris Harris <chris.harris@kitware.com>
2014-04-24 11:05:00 -0400

    Revert "Fixing PyQt to use superbuild sip"
    This reverts commit 21c4da10d316b77ea961849e7cedd57a977f9251.
    
    Conflicts:
    	CMake/cdat_modules/pyqt_external.cmake

--- CMake/cdat_modules/pyqt_external.cmake
+++ CMake/cdat_modules/pyqt_external.cmake
@@ -2,23 +2,7 @@
 #
 set(PyQt_source "${CMAKE_CURRENT_BINARY_DIR}/build/PyQt")
 message("[INFO] Using environment PYTHONPATH \"$ENV{PYTHONPATH}\"")
-
-set(sip_token "")
-set(sip_include_dir_token "")
-
-if (SB_ENABLE_SIP STREQUAL "ON")
-  set(sip_token "--sip=${sip_binary_dir}/sip")
-  set(sip_include_dir_token "--sip-incdir=${sip_include_dir}")
-endif()
-
-set(PyQt_configure_command env PYTHONPATH=$ENV{PYTHONPATH} ${PYTHON_EXECUTABLE} configure-ng.py
-      -q ${QT_QMAKE_EXECUTABLE} --confirm-license -b ${CMAKE_INSTALL_PREFIX}/bin
-      -d ${PYTHON_SITE_PACKAGES} -v ${CMAKE_INSTALL_PREFIX}/include
-      -v ${CMAKE_INSTALL_PREFIX}/share --designer-plugindir ${CMAKE_INSTALL_PREFIX}/share/plugins
-      -n ${CMAKE_INSTALL_PREFIX}/share/qsci ${sip_token} ${sip_include_dir_token}
-      --assume-shared -e QtGui -e QtHelp -e QtMultimedia -e QtNetwork -e QtDeclarative -e QtOpenGL
-      -e QtScript -e QtScriptTools -e QtSql -e QtSvg -e QtTest -e QtWebKit -e QtXml
-      -e QtXmlPatterns -e QtCore)
+set(PyQt_configure_command env PYTHONPATH=$ENV{PYTHONPATH} ${PYTHON_EXECUTABLE} configure.py -q ${QT_QMAKE_EXECUTABLE} --confirm-license -b ${CMAKE_INSTALL_PREFIX}/bin -d ${PYTHON_SITE_PACKAGES} -v ${CMAKE_INSTALL_PREFIX}/include -v ${CMAKE_INSTALL_PREFIX}/share -p ${CMAKE_INSTALL_PREFIX}/share/plugins -n ${CMAKE_INSTALL_PREFIX}/share/qsci --assume-shared -e QtGui -e QtHelp -e QtMultimedia -e QtNetwork -e QtDeclarative -e QtOpenGL -e QtScript -e QtScriptTools -e QtSql -e QtSvg -e QtTest -e QtWebKit -e QtXml -e QtXmlPatterns -e QtCore)
 
 ExternalProject_Add(PyQt
   DOWNLOAD_DIR ${CDAT_PACKAGE_CACHE_DIR}
@@ -29,4 +13,4 @@ ExternalProject_Add(PyQt
   CONFIGURE_COMMAND ${PyQt_configure_command}
   DEPENDS ${PyQt_deps}
   ${ep_log_options}
-)
+  )
--- CMake/cdat_modules/sip_external.cmake
+++ CMake/cdat_modules/sip_external.cmake
@@ -1,9 +1,4 @@
-set(sip_binary_dir ${CMAKE_INSTALL_PREFIX}/bin)
-set(sip_include_dir ${CMAKE_INSTALL_PREFIX}/include)
-set(sip_sip_dir ${CMAKE_INSTALL_PREFIX}/share)
-
-set(SIP_configure_command ${PYTHON_EXECUTABLE} ${cdat_BINARY_DIR}/build/SIP/configure.py -b ${sip_binary_dir}
-   -d ${PYTHON_SITE_PACKAGES} -e ${sip_include_dir} -v ${sip_sip_dir} CC=${CMAKE_C_COMPILER} CXX=${CMAKE_CXX_COMPILER})
+set(SIP_configure_command ${PYTHON_EXECUTABLE} ${cdat_BINARY_DIR}/build/SIP/configure.py -b ${CMAKE_INSTALL_PREFIX}/bin -d ${PYTHON_SITE_PACKAGES} -e ${CMAKE_INSTALL_PREFIX}/include -v ${CMAKE_INSTALL_PREFIX}/share CC=${CMAKE_C_COMPILER} CXX=${CMAKE_CXX_COMPILER})
 
 ExternalProject_Add(SIP
   DOWNLOAD_DIR ${CDAT_PACKAGE_CACHE_DIR}
