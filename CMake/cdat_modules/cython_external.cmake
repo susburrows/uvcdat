6a7949d8f8b62514abfead4942c201f988f69cc8
Chris Harris <chris.harris@kitware.com>
2014-05-12 12:08:30 -0400

    Ensure built Python is picked up for Cython build

--- CMake/cdat_modules/cython_external.cmake
+++ CMake/cdat_modules/cython_external.cmake
@@ -2,6 +2,12 @@
 #
 set(Cython_source "${CMAKE_CURRENT_BINARY_DIR}/build/Cython")
 
+set(_path $ENV{PATH})
+
+if (PYTHONINTERP STREQUAL "SYSTEM")
+  set(_path ${CMAKE_INSTALL_PREFIX}/bin:${_path})
+endif()
+
 ExternalProject_Add(Cython
   DOWNLOAD_DIR ${CDAT_PACKAGE_CACHE_DIR}
   SOURCE_DIR ${Cython_source}
@@ -9,7 +15,7 @@ ExternalProject_Add(Cython
   URL_MD5 ${CYTHON_MD5}
   BUILD_IN_SOURCE 1
   CONFIGURE_COMMAND ""
-  BUILD_COMMAND ${PYTHON_EXECUTABLE} setup.py build
+  BUILD_COMMAND env PATH=${_path} ${PYTHON_EXECUTABLE} setup.py build
   INSTALL_COMMAND ${PYTHON_EXECUTABLE} setup.py install ${PYTHON_EXTRA_PREFIX}
   DEPENDS ${Cython_deps}
   ${ep_log_options}
